4731c3b9ba0ba1b9f990ccdaa6845b65bc249e5d
yayan <muhainun059@gmail.com>
2023-03-04 14:47:30 +0700

    delete some stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1,70 +1,72 @@
-const html = document.querySelector('html');
-const darkToggle = document.getElementById('dark-toggle'); 
-const decor = document.getElementById('decor'); 
+const html = document.querySelector("html");
+const darkToggle = document.getElementById("dark-toggle");
+const decor = document.getElementById("decor");
 
-function a(){
-    if (darkToggle.checked) {
-    html.classList.add('dark')
-  }else{
-    html.classList.remove('dark')
+function a() {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
   }
 }
 
-window.onscroll = () =>{
-  const header = document.querySelector('#navbar'); 
-  const top = document.querySelector('#to-top'); 
+window.onscroll = () => {
+  const header = document.querySelector("#navbar");
+  const top = document.querySelector("#to-top");
   const fixednav = header.offsetTop;
-  if(window.pageYOffset > fixednav ){
-    header.classList.add('fixednav')
-    top.classList.add('flex')
-    top.classList.remove('hidden')
-  }else{
-    top.classList.remove('fixednav')
-    top.classList.add('hidden')
-    top.classList.remove('flex')
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("fixednav");
+    top.classList.add("flex");
+    top.classList.remove("hidden");
+  } else {
+    top.classList.remove("fixednav");
+    top.classList.add("hidden");
+    top.classList.remove("flex");
   }
-}
+};
 
-const burger = document.querySelector('.hamburger'); 
-const hidden= document.querySelector('.nav'); 
+const burger = document.querySelector(".hamburger1");
+const hidden = document.querySelector(".nav");
 function hamburger() {
-burger.classList.toggle('hamburger')
-hidden.classList.toggle('hidden')
+  burger.classList.toggle("hamburger");
+  hidden.classList.toggle("hidden");
 }
 
-
-
-
-;(function() {
+(function () {
   let tabs = document.querySelectorAll(".tabs");
-  tabs.forEach(function(tab) {
-      let tabTogglers = tab.querySelectorAll("a");
+  tabs.forEach(function (tab) {
+    let tabTogglers = tab.querySelectorAll("a");
 
-      tabTogglers.forEach(function(toggler) {
-          toggler.addEventListener("click", function(e) {
-            
-              e.preventDefault();
-              
-              let tabName = this.getAttribute("href");
+    tabTogglers.forEach(function (toggler) {
+      toggler.addEventListener("click", function (e) {
+        e.preventDefault();
 
-              let tabContents = document.querySelector("#" + tab.getAttribute("data-content"));
+        let tabName = this.getAttribute("href");
 
-              for (let i = 0; i < tabContents.children.length; i++) {
+        let tabContents = document.querySelector(
+          "#" + tab.getAttribute("data-content")
+        );
 
-                  tabTogglers[i].parentElement.classList.remove("border-blue-400", "border-b",  "opacity-100");
-                  tabContents.children[i].classList.remove("hidden");
-                  if ("#" + tabContents.children[i].id === tabName) {
-                      continue;
-                  }
-                  tabContents.children[i].classList.add("hidden");
-
-              }
-              e.target.parentElement.classList.add("border-blue-400", "border-b-3",  "opacity-100");
-          });
+        for (let i = 0; i < tabContents.children.length; i++) {
+          tabTogglers[i].parentElement.classList.remove(
+            "border-blue-400",
+            "border-b",
+            "opacity-100"
+          );
+          tabContents.children[i].classList.remove("hidden");
+          if ("#" + tabContents.children[i].id === tabName) {
+            continue;
+          }
+          tabContents.children[i].classList.add("hidden");
+        }
+        e.target.parentElement.classList.add(
+          "border-blue-400",
+          "border-b-3",
+          "opacity-100"
+        );
       });
+    });
 
-      tab.querySelectorAll(".default-tab")[0].click();
+    tab.querySelectorAll(".default-tab")[0].click();
   });
-})()
-
-
+})();
